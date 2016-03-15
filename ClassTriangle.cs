c5b59df8e590489958ca333124f47dcecc1646f9
levnikolaevich <levnikolaevich@ya.ru>
2016-03-15 19:38:10 +0300

    Dll

---
+++ ClassTriangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleDLL
+{
+    public class TriangleLN
+    {
+
+        /// <summary>
+        /// Triangle Square
+        /// </summary>
+        /// <param name="a">1st side</param>
+        /// <param name="b">2d side</param>
+        /// <param name="c">3d side</param>
+        public static double TriangleSQ(double a, double b, double c)
+        {
+            double s = 0;
+
+            if (a > b && a > c)
+            {
+
+                if (Math.Pow(a, 2) == Math.Pow(b, 2) + Math.Pow(c, 2))
+                    s = 0.5 * b * c;              
+
+            }
+            else if (b > a && b > c)
+            {
+                if (Math.Pow(b, 2) == Math.Pow(a, 2) + Math.Pow(c, 2))
+                    s = 0.5 * a * c;
+              
+            }
+            else
+            {
+                if (Math.Pow(c, 2) == Math.Pow(a, 2) + Math.Pow(b, 2))
+                    s = 0.5 * a * b;                
+            }               
+
+            return s;
+        }
+    }
+}
---
+++ TriangleDLLTests/TriangleLNTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleDLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleDLL.Tests
+{
+    [TestClass]
+    public class TriangleLNTests
+    {
+        [TestMethod]
+        public void TriangleSQTest()
+        {
+            
+            //Act
+
+            double result = TriangleLN.TriangleSQ(3,4,5);
+
+            //Assert
+            Assert.AreEqual(6, result);
+        }
+    }
+}
